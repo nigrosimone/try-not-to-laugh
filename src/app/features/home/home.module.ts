7de7737616b726a751bf5fc732ba2ff9affdaf69
Nigro Simone <nigro.simone@gmail.com>
2021-01-13 20:27:13 +0100

    up

--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -3,14 +3,15 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
     CommonModule,
     HomeRoutingModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    MatListModule
   ],
   exports: [
     HomeComponent
