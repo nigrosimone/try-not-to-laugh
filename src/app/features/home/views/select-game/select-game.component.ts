289af96f77659dc05a79cc48c68af2e34f509f68
Nigro Simone <nigro.simone@gmail.com>
2021-01-17 07:50:23 +0100

    fix typo

--- src/app/features/home/views/select-game/select-game.component.ts
+++ src/app/features/home/views/select-game/select-game.component.ts
@@ -1,22 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RoutingService } from 'src/app/core/services/routing/routing.service';
 import { DialogAlertService } from 'src/app/shared/components/dialog-alert/dialog-alert.service';
 
-const DIALOG_DESC_ARCADE = 'In questa DEMO ti vedrai un video divertente, cerca di non ridere!';
+const DIALOG_DESC_ARCADE = 'In questa DEMO vedrai un video divertente, cerca di non ridere!';
 const DIALOG_DESC_FACEDETECTION = 'In questa DEMO puoi TESTARE il riconoscimento delle espressioni facciali';
 @Component({
   selector: 'app-select-game',
   templateUrl: './select-game.component.html',
   styleUrls: ['./select-game.component.scss']
 })
-export class SelectGameComponent implements OnInit {
+export class SelectGameComponent {
 
   constructor(
     private routingService: RoutingService,
     private dialogAlertService: DialogAlertService) { }
-
-  ngOnInit(): void {
-  }
 
   onFaceExpressionDetectionTest(): void {
     this.dialogAlertService.openDialog({ title: 'Info', content: DIALOG_DESC_FACEDETECTION }).afterClosed().subscribe(result => {
