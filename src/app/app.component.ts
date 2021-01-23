1c75783d88802de922ae85bf1017cc2a198d490d
Nigro Simone <nigro.simone@gmail.com>
2021-01-23 18:36:35 +0100

    aggiunto pagina vista google analitycs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-
+import { NavigationEnd, Router } from '@angular/router';
+// tslint:disable-next-line: ban-types
+declare var gtag: Function;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,5 +9,16 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
+  constructor(public router: Router) {
 
+    // subscribe to router events and send page views to Google Analytics
+    this.router.events.subscribe(event => {
+
+      if (event instanceof NavigationEnd) {
+        if ( typeof gtag !== 'undefined' ) {
+          gtag('config', 'G-E03CYT4PX8', {page_path: event.urlAfterRedirects});
+        }
+      }
+    });
+  }
 }
