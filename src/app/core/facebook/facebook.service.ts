e86378eb213eabffb9b5fa7c488c997425227482
giovanni fiorentino <gtinto@gmail.com>
2021-01-24 21:49:27 +0100

    lista degli amici

--- src/app/core/facebook/facebook.service.ts
+++ src/app/core/facebook/facebook.service.ts
@@ -1,17 +1,31 @@
 import { Injectable } from '@angular/core';
+import { AuthenticationService } from '../services/auth/auth.service';
 
 @Injectable({ providedIn: 'root' })
 export class FacebookService {
-      private fb;
-      constructor(){
-          this.fb = (window as any).FB;
-      }
+  private fb;
+  constructor(public authService: AuthenticationService) {
+    this.fb = (window as any).FB;
+  }
 
-      inviteAFriend(): void  {
-        this.fb.ui({
-          method: 'send',
-          link: 'https://try-not-to-laugh.herokuapp.com/',
-        });
+  inviteAFriend(): void {
+    this.fb.ui({
+      method: 'send',
+      link: 'https://try-not-to-laugh.herokuapp.com/',
+    });
+  }
+
+  loadFriendList(): void {
+    this.fb.api(
+      `/${this.authService.currentUserValue.id}/friends`,
+      'GET',
+      {access_token: this.authService.currentUserValue.accessToken},
+      (response) => {
+        if (response && !response.error) {
+          console.log(response);
+        }
       }
+    );
+  }
 
 }
