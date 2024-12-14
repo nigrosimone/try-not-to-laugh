cf72d4542acdf714eefe887da5f5926df66ee5b7
Nigro Simone <nigro.simone@gmail.com>
2024-12-14 14:37:49 +0100

    clean up

--- src/app/shared/utils/common.ts
+++ src/app/shared/utils/common.ts
@@ -1,20 +1,8 @@
-import { Subscription } from 'rxjs';
-
-
 /**
  * Dato un array, ne restituisce un elemento a caso
  */
 export function randomItemFromArray<T>(value: Array<T>): T {
     return value[Math.floor(Math.random() * value.length)];
-}
-
-/**
- * Disiscrive una sottoscrizione se esiste
- */
-export function safeUnsubscribe(subscription: Subscription): void {
-    if (subscription) {
-        subscription.unsubscribe();
-    }
 }
 
 /**
