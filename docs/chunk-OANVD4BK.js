04bbfb08cf6c3cde6388ef6d3331a56b1caae2a0
Nigro Simone <nigro.simone@gmail.com>
2024-12-15 08:01:50 +0100

    up

---
+++ docs/chunk-OANVD4BK.js
@@ -0,0 +1,1 @@
+import"./chunk-TSRGIXR5.js";var t=[{path:"",loadComponent:()=>import("./chunk-34RAFI5H.js").then(o=>o.SelectGameComponent)}];export{t as routes};
--- docs/chunk-RGNPNEYD.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-TSRGIXR5.js";var t=[{path:"",loadComponent:()=>import("./chunk-GZ47FUHF.js").then(o=>o.SelectGameComponent)}];export{t as routes};
