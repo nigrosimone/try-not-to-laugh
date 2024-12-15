fe1369df0cbd5d43f6c512d8413a1acbc890331f
Nigro Simone <nigro.simone@gmail.com>
2024-12-15 09:52:40 +0100

    up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,7 +16,7 @@ export class AppComponent {
 
   constructor() {
     // subscribe to router events and send page views to Google Analytics
-    if (typeof gtag !== 'undefined') {
+    if (typeof gtag === 'function') {
       this.router.events.pipe(takeUntilDestroyed()).subscribe(event => {
         if (event instanceof NavigationEnd) {
           gtag('config', 'G-E03CYT4PX8', { page_path: event.urlAfterRedirects });
