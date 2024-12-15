a8f7683b0229fa90e4b69d7317d0db1385725d3a
Nigro Simone <nigro.simone@gmail.com>
2024-12-15 17:18:54 +0100

    clean up

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,5 +1,4 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { RoutingService } from 'src/app/core/services/routing/routing.service';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { RouterOutlet } from '@angular/router';
 
@@ -15,13 +14,5 @@ import { RouterOutlet } from '@angular/router';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HomeComponent {
-  private routingService = inject(RoutingService);
 
-  onFaceExpressionDetectionTest(): void {
-    this.routingService.homeCameraDetection();
-  }
-
-  onArcade(): void {
-    this.routingService.homeArcade();
-  }
 }
