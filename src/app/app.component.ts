d4fe3aac354b1ebb4b2a815bd355c805fafb12af
Nigro Simone <nigro.simone@gmail.com>
2024-12-15 13:27:09 +0100

    tolto gtag

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,5 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
-// tslint:disable-next-line: ban-types
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-declare const gtag: any;
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,16 +8,4 @@ declare const gtag: any;
   imports: [RouterOutlet],
 })
 export class AppComponent {
-  router = inject(Router);
-
-  constructor() {
-    // subscribe to router events and send page views to Google Analytics
-    if (typeof gtag === 'function') {
-      this.router.events.pipe(takeUntilDestroyed()).subscribe(event => {
-        if (event instanceof NavigationEnd) {
-          gtag('config', 'G-E03CYT4PX8', { page_path: event.urlAfterRedirects });
-        }
-      });
-    }
-  }
 }
